37aa9a9d666e09a04f4a31ebdc74db36b7416f4d
Pavel Kvasnička <94805399+pavel281185@users.noreply.github.com>
2024-03-13 17:43:11 +0100

    Create Java_TimBuchalkaMasterClass_DecimalComparator.java

---
+++ Java_TimBuchalkaMasterClass_DecimalComparator.java
@@ -0,0 +1,9 @@
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double a, double b){
+        if((int)(a * 1000) == (int)(b * 1000)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
